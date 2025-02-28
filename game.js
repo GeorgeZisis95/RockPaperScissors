0de9ccb3b74b99f068b419051124d794f6830e3f
__zisis__ <georgezisis@gmail.com>
2025-02-28 11:49:12 +0200

    Add ui support for game

--- game.js
+++ game.js
@@ -1,101 +1,49 @@
-function getComputerChoice() {
+scoreDiv = document.getElementsByClassName("score")[0];
+resultDiv = document.getElementsByClassName("result")[0];
+bestOfFiveDiv = document.getElementsByClassName("bestOfFive")[0];
 
-    let randomNum = Math.floor(Math.random() * 3);
+function playRound(playerChoice) {
 
-    if (randomNum === 0) {
-        return "rock";
+    const viableChoices = ["rock", "paper", "scissors"];
+    let theComputerChoice = viableChoices[Math.floor(Math.random() * viableChoices.length)];   
+
+    console.log(`Player picks: ${playerChoice} and Computer picks: ${theComputerChoice}`);
+
+    if (playerChoice === theComputerChoice){
+        result = "It's a tie";
     }
-    else if (randomNum === 1){
-        return "scissors";
-    }
-    else {
-        return "paper";
-    }
-}
-
-function caseInsensitive(theInput) {
-    return (
-
-        "rock".localeCompare(theInput, "en", {sensitivity:"accent"}) === 0 ||
-        "scissors".localeCompare(theInput, "en", {sensitivity:"accent"}) === 0 ||
-        "paper".localeCompare(theInput, "en", {sensitivity:"accent"}) === 0
-
-    );
-}
-
-function getPlayerChoice() {
-
-    let theChoice = prompt("Choose between rock paper or scissors:");
-
-    if (caseInsensitive(theChoice)) {
-        return theChoice.toLowerCase();
-    }
-    else {
-        console.assert(caseInsensitive(theChoice), "The input string is not compatible");
-        return "invalid input";
-    }
-}
-
-function playRound(playerChoice, computerChoice) {
-
-    if (playerChoice != "invalid input") {
-        console.log(`You picked ${playerChoice} and computer picks ${computerChoice}...`);
-    }
-
-    if (playerChoice === computerChoice){
-        return "IT'S A TIE";
-    }
-    else{
-        switch(playerChoice) {
-            case "rock":
-                return (computerChoice === "scissors") ? "YOU WIN" : "YOU LOSE";
-                
-            case "paper":
-                return (computerChoice === "rock") ? "YOU WIN" : "YOU LOSE";
-                
-            case "scissors":
-                return (computerChoice === "paper") ? "YOU WIN" : "YOU LOSE";  
-            
-            case "invalid input":
-                return "Please choose a valid input";
+    else if (
+            (playerChoice === "rock" && theComputerChoice === "scissors") || 
+            (playerChoice === "scissors" && theComputerChoice === "paper") ||
+            (playerChoice === "paper" && theComputerChoice === "rock")
+        ) {
+            result = "You win";
+            playerScore++;
         }
+    else {
+        result = "You lose";
+        computerScore++;
     }
-}
-
-function gameLoop() {
-    const playerSelection = getPlayerChoice(); 
-    const computerSelection = getComputerChoice();
-
-    let playerScore = 0; 
-    let computerScore = 0;
     
-    while (true) {
-
-        let result = playRound(playerSelection, computerSelection);
-
-        if (result === "YOU WIN") {
-                playerScore++;
-        }
-        if (result == "YOU LOSE") {
-            computerScore++;
-        }
-
-        console.log(`${result} Player: ${playerScore} - Computer: ${computerScore}`);
-
-        if (playerScore == 3 || computerScore == 3) {
-            break;
-        }
+    if (playerScore === 3 || computerScore === 3) {
+        resultDiv.textContent = `Player picks: ${playerChoice} and Computer picks: ${theComputerChoice} => ${result}`;
+        scoreDiv.textContent = `${playerScore} - ${computerScore}`;
+        bestOfFiveDiv.textContent = (playerScore === 3) ? "Player won the best of five" : "Computer won the best of five"
+        playerScore = 0; computerScore = 0;
     }
+    else {
+        bestOfFiveDiv.textContent = "";
+        resultDiv.textContent = `Player picks: ${playerChoice} and Computer picks: ${theComputerChoice} => ${result}`;
+        scoreDiv.textContent = `${playerScore} - ${computerScore}`;
+    } 
 }
 
-// gameLoop()
+let playerScore = 0;
+let computerScore = 0;
 
-const computerSelection = getComputerChoice();
-
-const btnRock = document.querySelector("#btnRock");
-const btnPaper = document.querySelector("#btnPaper");
-const btnScissors = document.querySelector("#btnScissors");
-
-btnRock.addEventListener("click", () => playRound("rock", computerSelection));
-btnPaper.addEventListener("click", () => playRound("paper", computerSelection));
-btnScissors.addEventListener("click", () => playRound("scissors", computerSelection));
+let buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound(button.getAttribute("data-choice"));
+    })
+})
